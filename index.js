cec4e9ce9e60df73ad4cf754014a944164aabd92
anonymous <101496703+themelodyman77@users.noreply.github.com>
2023-03-09 18:10:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ if(vp_check_mobile.matches)     // checks for mobile viewport
         }
     }
     //we hide the emoji on click, remember not to worry clicking emoji may result in varying behaviour
-    if(window.location.pathname === "/hideit/about.html")
+    if(window.location.pathname === "/about.html")
     {
         console.log("In about page");
         document.getElementById("hobbies").addEventListener("click", (event) =>
@@ -135,6 +135,11 @@ if(vp_check_mobile.matches)     // checks for mobile viewport
             });
         });
     }
+    // Light mode and Dark mode
+    document.getElementById("toggle_btn").addEventListener("click", function ()
+    {
+        document.getElementById("bdy").classList.toggle("light");
+    });
 }
 else{
     // Feedback function computer
@@ -172,7 +177,7 @@ else{
             document.getElementById("description_about4").style.display = "none";
         }
     }
-    if(window.location.pathname === "/hideit/about.html")
+    if(window.location.pathname === "/about.html")
     {
         document.getElementById("hobbies").addEventListener("mouseover", (event) =>
         {
